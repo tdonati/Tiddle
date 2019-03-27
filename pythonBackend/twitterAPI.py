ac30bc0959ffd22746b94f6ab020822849dbc731
tdonati <tdonati@bu.edu>
2019-03-27 16:40:18 -0400

    Fixed postman code

--- pythonBackend/twitterAPI.py
+++ pythonBackend/twitterAPI.py
@@ -10,14 +10,19 @@ querystring = {"count":"2","user_id":"3248878103"}
 
 payload = ""
 headers = {
-    'Authorization': "OAuth oauth_consumer_key='5p9bfzHYNr4PCXalLFhIYs7Gu',oauth_token='1098970060058750976-BtdgeuYMW4sTIG1TJqhkGFRavesa0T',oauth_signature_method='HMAC-SHA1',oauth_timestamp='1553566408',oauth_nonce='H9uimVpi3G7',oauth_version='1.0',oauth_signature=fWXpm4ClhSOxb7G3mBmMxsWvfdU%3D",
+    'Authorization': "OAuth oauth_consumer_key="'5p9bfzHYNr4PCXalLFhIYs7Gu'",oauth_token="'1098970060058750976-BtdgeuYMW4sTIG1TJqhkGFRavesa0T'",oauth_signature_method="'HMAC-SHA1'",oauth_timestamp="'1553718504'",oauth_nonce="'0bPafp6WBqU'",oauth_version="'1.0'",oauth_signature="'azM2atGo7WiIK74lQYaHl0SXNoY%3D'"",
     'cache-control': "no-cache",
-    'Postman-Token': "ec5cc3be-828f-44b7-ae5a-927585f2b26b"
+    'Postman-Token': "a8b86ecf-cb87-480a-adbf-6cedba7a2476"
     }
+
 
 response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
 
 print(response.text)
+
+for i in response:
+    if i == 'text':
+        print(i)
 
 auth = tweepy.OAuthHandler(secret.CONSUMER_KEY, secret.CONSUMER_SECRET)
 auth.set_access_token(secret.ACCESS_TOKEN, secret.ACCESS_TOKEN_SECRET)
