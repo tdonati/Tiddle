ec44caa1d71fa492ebe74351e35baecc5bf61b45
skyemck <30445669+skyemck@users.noreply.github.com>
2019-03-28 13:39:36 -0400

    Create test.js

---
+++ pythonBackend/templates/scripts/test.js
@@ -0,0 +1,10 @@
+var myButton = document.querySelector('button');
+
+myButton.onclick = function(){
+	getTwitterName();
+}
+
+function getTwitterName(){
+	var myName = prompt('Please enter your Twitter name.');
+	
+}
