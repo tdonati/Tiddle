8dd0e91c6d6c3ca1ed4b7684d1141f32532bba69
skyemck <30445669+skyemck@users.noreply.github.com>
2019-03-28 13:58:21 -0400

    Update test.js

--- pythonBackend/templates/scripts/test.js
+++ pythonBackend/templates/scripts/test.js
@@ -1,8 +1,8 @@
 var myButton = document.querySelector('button');
 
-myButton.onclick = function(){
-	getTwitterName();
-}
+//myButton.onclick = function(){
+//	getTwitterName();
+//}
 
 function getTwitterName(){
 	var myName = prompt('Please enter your Twitter name.');
