fa1edfccd18cc6bc73ca90bf88804747d94004eb
skyemck <30445669+skyemck@users.noreply.github.com>
2019-04-25 22:59:38 -0400

    Create db.py

---
+++ pythonBackend/db.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client['test_db']
+
+
+def insert(value, collection):
+    col = db[collection]
+    insert_id = col.insert_one(value)
+    return insert_id
+
+def update(value, collection):
+    col = db[collection]
+    insert_id = col.update({'_id': value['_id']}, value, upsert=True)
+    return insert_id
+
+
+def search(key, collection):
+    col = db[collection]
+    data = col.find_one(key)
+    return data
+
+
+def search_many(key, collection):
+    col = db[collection]
+    data = col.find({"location": "Boston, Mass."})
+    return [x for x in data]
