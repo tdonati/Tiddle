45feb7e3999be7d0b453282cbe778eb75b7be3be
skyemck <30445669+skyemck@users.noreply.github.com>
2019-04-27 15:21:00 -0400

    Update user.py

--- pythonBackend/user.py
+++ pythonBackend/user.py
@@ -8,21 +8,47 @@ from flask import json,Flask,render_template,request,redirect,url_for
 from flask.json import dumps
 from db import insert, search, update
 
-auth = tweepy.OAuthHandler(secret.CONSUMER_KEY, secret.CONSUMER_SECRET)
-auth.set_access_token(secret.ACCESS_TOKEN, secret.ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
 
 class User:
     def __init__(self, username):
+        
+        auth = tweepy.OAuthHandler(secret.CONSUMER_KEY, secret.CONSUMER_SECRET)
+        auth.set_access_token(secret.ACCESS_TOKEN, secret.ACCESS_TOKEN_SECRET)
+        api = tweepy.API(auth)
+
+        
         self.username = username
+        self.sent = 0
         self.user = api.get_user(username)
         self.user_id = self.user.id
         self.tweets= ""
+        self.movie = []
+        self.genre = ""
+        self.rec_list = {
+        			'Action': [],
+        			'Thriller': [],
+        			'Comedy': [],
+        			'Animation': [],
+        			'Adventure': [],
+        			'Family': [],
+        			'Musical': [],
+        			'Horror': [],
+        			'Romance': [],
+        			'Biography': [],
+        			'Sci-Fi': [],
+        			'Drama':[],
+        			'Mystery':[],
+        			'Fantasy':[],
+        			'Documentary':[]
+        			}
 
         
         
 
     def get_data(self):
+        auth = tweepy.OAuthHandler(secret.CONSUMER_KEY, secret.CONSUMER_SECRET)
+        auth.set_access_token(secret.ACCESS_TOKEN, secret.ACCESS_TOKEN_SECRET)
+        api = tweepy.API(auth)
 
     # check if user exists
     #try:
@@ -57,13 +83,27 @@ class User:
         
 
 
-    def process_tweet(self, tweet):
-        return {
-            "time": tweet.created_at.strftime('%m/%d/%y'),
-            "text": tweet.text,
-            "_id": tweet.id,
-            "user_id": self.user_id
-        }
+    def update_recList(self):
+    	dic = {}
+    	option = ['Action','Thriller','Comedy','Animation',
+    	'Adventure','Family','Musical','Horror','Romance','Biography',
+    	'Sci-Fi','Drama','Mystery','Fantasy','Documentary']
+    	for i in option:
+    		if i == self.genre:
+    			dic[i] = self.movie
+    		else :
+    			dic[i] = self.rec_list.get(i)
+    	self.rec_list = dic
+    	insert_tweet({
+    		'_id': "u_" + str(self.user_id),
+            'tweets': self.tweets,
+            'username': self.username,
+            'user': self.user._json,
+            'movie': self.movie,
+            'rec_list':self.rec_list
+    		})
+    	return
+         
 
     def get_tweets(self):
         tweets = self.get_data()
@@ -71,8 +111,10 @@ class User:
         insert_tweet({
             '_id': "u_" + str(self.user_id),
             'tweets': self.tweets,
-            'username': self.username[1:],
-            'user': self.user._json
+            'username': self.username,
+            'user': self.user._json,
+            'movie': self.movie,
+            'rec_list':self.rec_list
             })
         return
 
@@ -80,8 +122,10 @@ class User:
     	insert_user({
     		'_id': "u_" + str(self.user_id),
             'tweets': self.tweets,
-            'username': self.username[1:],
-            'user': self.user._json
+            'username': self.username,
+            'user': self.user._json,
+            'movie': self.movie,
+            'rec_list':self.rec_list
     		})
 
     
@@ -92,7 +136,9 @@ class User:
         return dumps({
             'username': self.username,
             'user_id': self.user_id,
-            'tweets': self.tweets
+            'tweets': self.tweets,
+            'movie': self.movie,
+            'rec_list':self.rec_list
         })
 
 class Search:
@@ -117,11 +163,26 @@ def cleanTweet(tweet):
     text = text.replace(u"\u2018", "'")
     text = text.replace(u"\u2019", "'")
     text = text.replace(u"\u2026", "'")
-    text = text.replace(u"\u2014", "'")
-    text = text.replace(u"\U0001f60f", "'")
-    text = text.replace(u"\U0001f924", "'")
+    text = text.replace('"', "")
+    text = text.replace("'", "")
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               "]+", flags=re.UNICODE)
+    text = emoji_pattern.sub(r'', text)
+    alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    finalText = ""
 
-    return text
+    for char in text:
+        if char in string.punctuation or char in alphabet or char == " ":
+            finalText = finalText + char
+
+    #print(finalText)
+    #print("HELLOEOAFHEOAHFOIEAHOFIHAEOIWHFOAEWHFOEAHWOFHEAOWHF")
+    #print(text)
+    return finalText
 
 def get_user(username):
     """
@@ -140,3 +201,4 @@ def get_user(username):
 if __name__ == '__main__':
     u = User('@gaspardetienne9')
     print(u.user_id)
+
